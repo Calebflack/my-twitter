17ad2fa60b9d916e0990338b15dfbb5dbc3e9e71
Calebflack <caleb_2020@qq.com>
2021-12-27 07:21:11 +0800

    initial commit

--- components/Feed.js
+++ components/Feed.js
@@ -38,6 +38,7 @@ function Feed() {
 
   return (
     <div className="flex-grow border-l border-r border-gray-700 max-w-2xl sm:ml-[73px] xl:ml-[370px]">
+      <h4 className=" font-bold text-center text-white font-sans text-black hover:text-orange-700 bg-white"> THIS IS NOT TWITTER 不是真的</h4>
       <div className="text-[#d9d9d9] flex items-center sm:justify-between py-2 px-3 sticky top-0 z-50 bg-black border-b border-gray-700">
         <h2 className="text-lg sm:text-xl font-bold">Home</h2>
         <div className="hoverAnimation w-9 h-9 flex items-center justify-center xl:px-0 ml-auto">
